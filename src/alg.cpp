3e776892bad1d1d1736577b6aae40dbdf45c2ca7
DegtyarevPaul <99130466+DegtyarevPaul@users.noreply.github.com>
2022-03-28 00:49:48 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,22 +1,22 @@
 // Copyright 2021 DegtyarevPaul
-int countPairs1(int *arr, int len, int value) {
+int countPairs1(int* arr, int len, int value) {
 int c = 0;
 for (int i = 0; i <= len; i++) {
 for (int j = 0; j <= len; j++) {
-if (*(arr + i) + *(arr+j) == value && i != j)
+if (*(arr + i) + *(arr + j) == value && i != j)
 c++;
 }
 }
 return c / 2;
 }
 
-int countPairs2(int *arr, int len, int value) {
+int countPairs2(int* arr, int len, int value) {
 int i = 0;
 int j = len - 1;
 int c = 0;
 for (i = 0; i < len; i++) {
 for (j = len - 1; j > i; j--) {
-if (*(arr+i) + *(arr+j) == value)
+if (*(arr + i) + *(arr + j) == value)
 c++;
 }
 }
